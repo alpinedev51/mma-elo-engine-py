7caa80946838ae04cde7f080be2edb66acef1eea
Austin Barton <101911867+abarton51@users.noreply.github.com>
2024-10-12 16:04:07 -0400

    fix processing

--- backend/app/database.py
+++ backend/app/database.py
@@ -16,8 +16,8 @@ Base = declarative_base()
 def init_db():
     Base.metadata.create_all(bind=engine)
     # Optionally check for created tables
-    # inspector = inspect(engine)
-    # print("Tables in database:", inspector.get_table_names())
+    inspector = inspect(engine)
+    print("Tables in database:", inspector.get_table_names())
     
 def get_db():
     db = SessionLocal()

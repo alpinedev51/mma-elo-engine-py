f6729c266974ecb460d0626e777f17b1b748f54b
Austin Barton <101911867+abarton51@users.noreply.github.com>
2024-12-12 11:48:57 -0500

    add in sub vs ko/tko logic

--- backend/app/elo_engine/elo_calculator.py
+++ backend/app/elo_engine/elo_calculator.py
@@ -17,7 +17,7 @@ def calculate_elo(fighter_1_elo: Union[float, int], fighter_2_elo: Union[float, 
         S_fighter_1 = 0.5
         S_fighter_2 = 0.5
     else:
-        return fighter_1_elo, fighter_2_elo  # No change for no contest (nc)
+        return fighter_1_elo, fighter_2_elo
 
     fighter_1_elo_new = fighter_1_elo + k_factor * (S_fighter_1 - expected_score_fighter_1)
     fighter_2_elo_new = fighter_2_elo + k_factor * (S_fighter_2 - expected_score_fighter_2)
@@ -27,7 +27,14 @@ def expected_score(current_elo: Union[float, int], opponent_elo: Union[float, in
     return 1 / (1 + 10**((opponent_elo - current_elo)/denom))
 
 def get_k_factor(method: str, k: Union[float, str], multiplier: Union[float, int]):
-    if method == 'KO/TKO' or method == 'SUB':
-        return k * multiplier  # Increase K by 15% for KO or submission
+    if method == 'KO/TKO':
+        return k * multiplier
+    elif method == 'SUB':
+        multiplier_increase = multiplier - 1
+        # should not be < 0. If == 0, then multiplier results in no change
+        if multiplier_increase <= 0:
+            return k * multiplier
+        else:
+            return k * (1 + (multiplier_increase * 0.5))
     else:
         return k
